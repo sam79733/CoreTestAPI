09c4b58a34ba3f9f1295faad2ce9d453e8dca907
Gyanendra <gyanendra.cse@gmail.com>
2021-06-03 00:32:57 +0530

    3rd commit

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace CoreAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. test Use this method to add services to the container.
+        // This method gets called by the runtime.  Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
